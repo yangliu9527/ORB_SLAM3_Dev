0b028b1e86da801d5fbde07768adf9e3f368d4f1
liuyang <liuyangmail@whu.edu.cn>
2024-12-14 03:43:00 +0800

    for station backup ubuntu20

--- Examples_added/Monocular-Inertial/mono_inertial_kitti_added.cc
+++ Examples_added/Monocular-Inertial/mono_inertial_kitti_added.cc
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
                     first_imu++;
                 }
 
-    #ifdef COMPILEDWITHC11
+    #if (defined COMPILEDWITHC11) || (defined COMPILEDWITHC14)
             std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     #else
             std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
@@ -130,7 +130,7 @@ int main(int argc, char **argv)
             // Pass the images to the SLAM system
             SLAM.TrackMonocular(imLeft,tframe,vImuMeas);
 
-    #ifdef COMPILEDWITHC11
+    #if (defined COMPILEDWITHC11) || (defined COMPILEDWITHC14)
             std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     #else
             std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
