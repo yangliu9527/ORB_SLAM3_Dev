6767e6ef144cb98a92098bf9a62bcc5542dab684
liuyang <liuyangmail@whu.edu.cn>
2024-12-14 18:29:44 +0800

    backup for station,ubuntu20,opencv4

--- run_euroc.py
+++ run_euroc.py
@@ -13,7 +13,7 @@ for i in range(0,len(seq_names)):
     seq_name = seq_names[i]
     #run the system
     print(f"#######################evaluating {seq_name} seq camera motion#########################")
-    #os.system(f'./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml {dataset_path}/{seq_name} Config/EuRoC/EuRoC_TimeStamps/{seq_name}.txt') 
+    os.system(f'./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml {dataset_path}/{seq_name} Config/EuRoC/EuRoC_TimeStamps/{seq_name}.txt') 
     os.system(f'cp CameraTrajectory.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt')
     print(f"------------evaluating {seq_name} seq ATE------------")
     os.system(f'python2 ./Evaluation/TUM_Tools/evaluate_ate.py {evaluation_workspace}/ground_truth/{seq_name}_GT.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt  --verbose --plot {evaluation_workspace}/results/{seq_name}.png --save_file {evaluation_workspace}/results/eva_{seq_name}.txt')
