d3cee854031cac298098aded98a399d7d2734d66
liuyang9527 <liuyangmail@whu.edu.cn>
2025-04-18 14:34:29 +0800

    add and modify dataset supporting

--- run_euroc.py
+++ run_euroc.py
@@ -8,17 +8,17 @@ plot_flag = parser.parse_args().plot
 dataset_path = "/home/zhiyu/DataSet/EuRoC/"
 evaluation_workspace = "./Evaluation/EuRoC/"
 # seq_names = ['MH01', 'MH02', 'MH03',  'MH04','MH05','V101','V102','V103','V201','V202','V203']
-seq_names = ['MH01']
+seq_names = ['MH01','V101']
 for i in range(0,len(seq_names)):
     seq_name = seq_names[i]
     #run the system
     print(f"#######################evaluating {seq_name} seq camera motion#########################")
-    os.system(f'./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml {dataset_path}/{seq_name} Config/EuRoC/EuRoC_TimeStamps/{seq_name}.txt') 
-    os.system(f'cp CameraTrajectory.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt')
+    # os.system(f'./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml {dataset_path}/{seq_name} Config/EuRoC/EuRoC_TimeStamps/{seq_name}.txt') 
+    # os.system(f'cp CameraTrajectory.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt')
     print(f"------------evaluating {seq_name} seq ATE------------")
-    os.system(f'python2 ./Evaluation/TUM_Tools/evaluate_ate.py {evaluation_workspace}/ground_truth/{seq_name}_GT.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt  --verbose --plot {evaluation_workspace}/results/{seq_name}.png --save_file {evaluation_workspace}/results/eva_{seq_name}.txt')
-    print(f"------------evaluating {seq_name} seq RPE------------")
-    os.system(f'python2 ./Evaluation/TUM_Tools/evaluate_rpe.py {evaluation_workspace}/ground_truth/{seq_name}_GT.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt  --delta_unit=f --fixed_delta --verbose --plot {evaluation_workspace}/results/{seq_name}_rpe.png --save_file {evaluation_workspace}/results/{seq_name}_result_rpe.txt')    
+    os.system(f'evo_ape euroc {evaluation_workspace}/ground_truth/{seq_name}_GT.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt  --no_warnings  --save_plot {evaluation_workspace}/results/{seq_name}_evo_ate')
+    print(f"------------evaluating {seq_name} traj------------")
+    os.system(f'evo_traj euroc --ref={evaluation_workspace}/ground_truth/{seq_name}_GT.txt {evaluation_workspace}/results/CameraTrajectory_{seq_name}.txt  --no_warnings    --plot_mode=xy  --save_plot {evaluation_workspace}/results/{seq_name}_evo_traj')    
    
 
 
--- run_tum_rgbd.py
+++ run_tum_rgbd.py
@@ -6,7 +6,7 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument("--cam_pose",type=int,default=1)
 parser.add_argument("--plot",type=int,default=1)
-parser.add_argument("--seq_id",type=str,default="rgbd_dataset_freiburg2_desk")
+parser.add_argument("--seq_id",type=str)
 parser.add_argument("--frame_num",type=int,default=500)
 
 ########################evaluation parameters########################
@@ -19,9 +19,10 @@ print(f'evaluate cam_pose:{eva_campose}, plot figures:{plot_flag}')
 
 ########################dataset and workspace path########################
 dataset_path = "/home/zhiyu/DataSet/TUM-RGBD/"
+evaluation_workspace = "./Evaluation/TUM-RGBD/"
 
-
-seq_ids =["rgbd_dataset_freiburg3_walking_xyz", "rgbd_dataset_freiburg2_desk"]
+#seq_ids =["rgbd_dataset_freiburg3_walking_xyz", "rgbd_dataset_freiburg2_desk"]
+seq_ids =["rgbd_dataset_freiburg3_walking_xyz", "rgbd_dataset_freiburg3_walking_static","rgbd_dataset_freiburg3_walking_halfsphere"]
 
 # select seq ids
 if (seq_id == None):
@@ -37,24 +38,7 @@ for seq_id in seq_ids:
     numbers = [int(num) for num in numbers]
     print(str(numbers[0]))
     os.system(f'./Examples_added/RGB-D/rgbd_tum_added Vocabulary/ORBvoc.txt ./Config/TUM-RGBD/TUM{str(numbers[0])}.yaml  {dataset_path}/{seq_id}/ {dataset_path}/{seq_id}/associate.txt')
-    # #####copy data to save
-    # os.system(f'cp ObjectTrajectory.txt {evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}.txt')
-    # os.system(f'cp CameraTrajectory.txt {evaluation_workspace}/results_ego/pose_est_{frame_num}_{seq_id}.txt')
-
-    # if(eva_campose):
-    #     print(f"#############################evaluating camera pose#############################")
-    #     os.system(f'python ./Evaluation/KITTI_Tools/evaluate_kitti.py --gt_file={evaluation_workspace}/ground_truth_ego/pose_gt_{frame_num}_{seq_id}.txt  --est_file={evaluation_workspace}/results_ego/pose_est_{frame_num}_{seq_id}.txt --output_dir={evaluation_workspace}/results_ego/ --align=6dof --seq_id=cam_{frame_num}_{seq_id}')
-
-    # if(eva_objpose):
-    #     relations = obj_gt_est_relation[seq_id]
-    #     for relation in relations:
-    #         gt_trackid = relation[0]
-    #         est_trackid = relation[1]
-    #         #######################object pose evaluation#######################
-    #         if(eva_objpose):
-    #             print(f"=======================================evaluating object {gt_trackid} pose=======================================")
-    #             print(f"=======================================evaluating object {gt_trackid} pose=======================================")
-    #             os.system(f'python {evaluation_workspace}/OMD_Tools/extract_objtraj.py {evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}.txt {seq_id} {est_trackid} {evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}_{est_trackid}.txt')
-    #             os.system(f'python {evaluation_workspace}/OMD_Tools/align_trajectory.py --gt_traj_path={evaluation_workspace}/ground_truth_object/pose_gt_{frame_num}_{seq_id}_{int(gt_trackid)}.txt --est_traj_path={evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}_{int(est_trackid)}.txt {seq_id} --gt_track_id={int(gt_trackid)} --est_track_id={int(est_trackid)} --gt_output_path={evaluation_workspace}/results_object/pose_gt_{frame_num}_{seq_id}_{int(gt_trackid)}_aligned.txt --est_output_path={evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}_{int(est_trackid)}_aligned.txt')
-    #             os.system(f'python ./Evaluation/KITTI_Tools/evaluate_kitti.py --gt_file={evaluation_workspace}/results_object/pose_gt_{frame_num}_{seq_id}_{int(gt_trackid)}_aligned.txt  --est_file={evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}_{int(est_trackid)}_aligned.txt --output_dir={evaluation_workspace}/results_object/ --align=6dof --seq_id={frame_num}_{seq_id}_{int(gt_trackid)}')
-    #             #os.system(f'python ./Evaluation/KITTI_Tools/evaluate_kitti.py --gt_file={evaluation_workspace}/results_object/pose_gt_{frame_num}_{seq_id}_{int(gt_trackid)}_aligned.txt  --est_file={evaluation_workspace}/results_object/pose_est_{frame_num}_{seq_id}_{int(est_trackid)}_aligned.txt --output_dir={evaluation_workspace}/results_object/ --seq_id={frame_num}_{seq_id}_{int(gt_trackid)}')
+    os.system(f'cp CameraTrajectory.txt {evaluation_workspace}/results/{seq_id}_est.txt')
+    print(f"#############################evaluating {seq_id} camera pose#############################")
+    os.system(f'evo_traj tum {evaluation_workspace}/results/{seq_id}_est.txt --ref={evaluation_workspace}/ground_truth/{seq_id}_gt.txt -a --no_warnings    --plot_mode=xy  --save_plot {evaluation_workspace}/results/{seq_id}_evo_traj')
+    os.system(f'evo_ape tum {evaluation_workspace}/ground_truth/{seq_id}_gt.txt {evaluation_workspace}/results/{seq_id}_est.txt -a --no_warnings  --save_plot {evaluation_workspace}/results/{seq_id}_evo_ate')  
